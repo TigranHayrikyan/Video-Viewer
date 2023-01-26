4fea1754e2d8b06586cd0cea89e96f0caae6a6c6
TigranHayrikyan <tigran.hayrikyan@gmail.com>
2023-01-26 14:11:29 +0400

    THis is a last version of simple video viewer

--- renderer.js
+++ renderer.js
@@ -5,3 +5,15 @@
  * `contextIsolation` is turned on. Use the contextBridge API in `preload.js`
  * to expose Node.js functionality from the main process.
  */
+
+function upload(){
+    var input = document.getElementById("file");
+    var reader = new FileReader();
+    reader.readAsDataURL(input.files[0]);
+    var fileName = input.files[0].name;
+    reader.onload = function(){
+        document.getElementById("video").src = reader.result;
+        document.getElementById("description").textContent = '"' + fileName + '"'  + " File is opened"; 
+        document.getElementById("description").style.color = "#ccc" ; 
+    };
+}
